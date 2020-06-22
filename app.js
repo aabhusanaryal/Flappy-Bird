3dd59697fc3f9d9756f0c601602d3270e1eb02ab
Aabhusan Aryal <aabhusanaryal@gmail.com>
2020-06-22 18:57:30 +0545

    Added function to add pipes, not random yet. Scrolls well

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // Selectors
+const gameContainer = document.querySelector(".gameContainer");
 const char = document.querySelector("#char");
-const pipeContainer = document.querySelector(".pipeContainer");
+const pipeContainer = document.querySelector(".gameContainer");
 // Other variables
-let charPosition = window.innerHeight / 2;
+let windowHeight = window.innerHeight;
+let charPosition = windowHeight / 2;
 let pipePosition = 500;
 // Functions
 function changeCharPos(newPos) {
@@ -12,14 +14,26 @@ function changeCharPos(newPos) {
 function changePipePos(newPos) {
     pipeContainer.style.left = `${newPos}px`;
 }
+
+function addPipe() {
+    const newPipeContainer = document.createElement("span");
+    const newPipeTop = document.createElement("div");
+    const newPipeBottom = document.createElement("div");
+    newPipeContainer.classList.add("pipeContainer");
+    newPipeTop.classList.add("pipe", "topPipe");
+    newPipeBottom.classList.add("pipe", "bottomPipe");
+    newPipeContainer.append(newPipeTop, newPipeBottom);
+    gameContainer.append(newPipeContainer);
+}
 //Setting the char position to mid of screen, initially
 changeCharPos(charPosition);
+
 // Game Loop
 setInterval(() => {
-    charPosition += 20;
-    changeCharPos(charPosition);
-    pipePosition -= 15;
-    changePipePos(pipePosition);
+    // charPosition += 20;
+    // changeCharPos(charPosition);
+    // pipePosition -= 15;
+    // changePipePos(pipePosition);
 }, 60);
 
 document.addEventListener("keydown", (event) => {
@@ -28,3 +42,8 @@ document.addEventListener("keydown", (event) => {
         changeCharPos(position);
     }
 });
+
+addPipe();
+addPipe();
+addPipe();
+addPipe();
