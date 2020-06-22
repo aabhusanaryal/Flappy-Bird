4083ad2018e3e02b58396a6d7c3530112c6795d8
Aabhusan Aryal <aabhusanaryal@gmail.com>
2020-06-22 20:37:17 +0545

    trying to remove and add pipes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const pipeContainer = document.querySelector(".gameContainer");
 let windowHeight = window.innerHeight;
 let charPosition = windowHeight / 2;
 let pipePosition = 500;
+let score = 0;
+
 // Functions
 function changeCharPos(newPos) {
     char.style.top = `${newPos}px`;
@@ -25,6 +27,27 @@ function addPipe() {
     newPipeContainer.append(newPipeTop, newPipeBottom);
     gameContainer.append(newPipeContainer);
 }
+
+function removePipe(i) {
+    let pipes = document.getElementsByClassName("pipeContainer");
+    gameContainer.removeChild(pipes[i]);
+}
+
+function gameOver() {
+    alert("Game Over!");
+}
+
+function checker() {
+    let i = 0; //index for pipes
+    const pipes = document.getElementsByClassName("pipeContainer");
+    const coorPipe = pipes[i].getBoundingClientRect(); //used to find coordinates relative to browser
+    if (coorPipe.left <= 0) {
+        removePipe(i);
+        addPipe();
+        console.log("delete");
+        // i++;
+    }
+}
 //Setting the char position to mid of screen, initially
 changeCharPos(charPosition);
 
@@ -34,16 +57,20 @@ setInterval(() => {
     // changeCharPos(charPosition);
     // pipePosition -= 15;
     // changePipePos(pipePosition);
+    // checker();
 }, 60);
 
 document.addEventListener("keydown", (event) => {
     if (event.key === " ") {
         charPosition -= 250;
-        changeCharPos(position);
+        changeCharPos(charPosition);
     }
 });
 
-addPipe();
-addPipe();
-addPipe();
-addPipe();
+// addPipe();
+// addPipe();
+// addPipe();
+// addPipe();
+// addPipe();
+// addPipe();
+// addPipe();
