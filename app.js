a3e1648016ea6b794ae1ca14e4c6404f4ae1336b
Aabhusan Aryal <aabhusanaryal@gmail.com>
2020-06-22 17:50:48 +0545

    Added pipes and made it move

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 // Selectors
 const char = document.querySelector("#char");
-
+const pipeContainer = document.querySelector(".pipeContainer");
+// Other variables
+let charPosition = window.innerHeight / 2;
+let pipePosition = 500;
 // Functions
 function changeCharPos(newPos) {
     char.style.top = `${newPos}px`;
-    char.style.backgroundColor = "pink";
 }
-let position = 0;
+
+function changePipePos(newPos) {
+    pipeContainer.style.left = `${newPos}px`;
+}
+//Setting the char position to mid of screen, initially
+changeCharPos(charPosition);
 // Game Loop
 setInterval(() => {
-    changeCharPos(position);
-    position += 30;
+    charPosition += 20;
+    changeCharPos(charPosition);
+    pipePosition -= 15;
+    changePipePos(pipePosition);
 }, 60);
 
-changeCharPos(500);
-// char.style.top = "100px";
-
 document.addEventListener("keydown", (event) => {
-    if ((event.key = " ")) {
-        position -= 250;
+    if (event.key === " ") {
+        charPosition -= 250;
         changeCharPos(position);
     }
 });
